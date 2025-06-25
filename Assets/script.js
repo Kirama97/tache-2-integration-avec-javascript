31d97849e3eaa52ebaa8a5d41b4ee1687fdc4aae
KIRAMA97 <officialthiam97@gmail.com>
2025-06-25 15:59:36 +0000

    hero en slide

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,4 @@
 
-let currentSlide = 0;
-const totalCards = 12;
-const visibleCards = 4;
-const maxSlide = totalCards - visibleCards;
-
-function slide(direction) {
-  currentSlide += direction;
-  if (currentSlide < 0) currentSlide = 0;
-  if (currentSlide > maxSlide) currentSlide = maxSlide;
-
-  const sliderTrack = document.getElementById('sliderTrack');
-  const slideWidth = sliderTrack.offsetWidth / visibleCards;
-  sliderTrack.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-}
-
-
 
 // burguer
 
